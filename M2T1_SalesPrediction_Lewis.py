9a7317cd3afe5bbf4dba9a8921364cf592f7017e
lewiss9581 <31832572+lewiss9581@users.noreply.github.com>
2017-09-10 19:53:04 -0400

    Add files via upload

---
+++ M2T1_SalesPrediction_Lewis.py
@@ -0,0 +1,19 @@
+#CTI-110
+#M2T1 - Sales Prediction
+#Sedrick Lewis
+#9/10/2017
+
+#Purpose of program is to calculate the projected total sales.
+
+#total sales variable is going to be set
+#user answer the below question
+
+
+total_sales = float(input('Enter the projected sales:'))
+
+#Calculate the profit as 23 percent of total sales
+
+profit = total_sales * 0.23
+
+#Display the profit
+print('The profit is $', profit)
