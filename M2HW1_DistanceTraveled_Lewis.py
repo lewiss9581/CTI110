107981a50288434560dac7aa69879bc65ed4d339
lewiss9581 <31832572+lewiss9581@users.noreply.github.com>
2017-09-10 20:13:28 -0400

    M2HW1_DistanceTraveled_Lewis.py
    Program purpose is to calculate Distance by multiplying speed and time

---
+++ M2HW1_DistanceTraveled_Lewis.py
@@ -0,0 +1,30 @@
+# CTI-110 
+ # M2HW1 - Distance Traveled  
+ # Lewis    
+ # 09/10/2017
+
+
+ 
+#Variables
+speed = float(70)
+time = float(6)
+
+#Caclucation Process
+distance6 = speed * time
+print(distance6)
+ 
+#Variables
+speed = float(70)
+time = float(10)
+
+#Caclucation Process
+distance10 = speed * time
+print(distance10)
+
+#Variables
+speed = float(70)
+time = float(15)
+
+#Caclucation Process
+distance15 = speed * time
+print(distance15)
